50b21daa0fa285741886377806dd62e18cf22183
John-Tenning <sasti244@gmail.com>
2023-01-05 12:06:28 +0530

    Added Login and Signup

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,23 @@
 module.exports = {
   mode: "jit",
   purge: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
-  theme: {},
+  theme: {
+    colors : {
+      'transparent' : 'transparent',
+      'current' : 'currentColor',
+      'black' : '#000',
+      'white' : '#fff',
+      'dark' : '#313236' ,
+      'grey' : '#F7F7F7' , 
+      'yellow' : '#FDDB00' , 
+      'navy-blue' : '#001AA0' , 
+      'fog' : '#B9B9B9' , 
+      'smoke' : '#D9D9D9'
+    } , 
+    fontFamily : {
+      lato : ['Lato', 'sans-serif']
+    }
+  },
   variants: {
     extend: {},
   },
