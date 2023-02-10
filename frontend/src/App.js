c53289091ccfc5fa6d9e8161c02408fef7918bf9
krithik2208 <krithik2208@gmail.com>
2023-02-10 09:55:03 +0530

    added profile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Login from "./components/Login";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Signup from "./components/Signup";
 import Home from "./components/HomePage";
+import Profile from "./components/Profile"
 
 // import { NavBar } from "@tgashwinkumar/microservices-library";
 const App = () => {
@@ -10,9 +11,12 @@ const App = () => {
     <div className="">
       <BrowserRouter>
         <Routes>
-          <Route index path="/" element={<Home />} />
+          <Route index path="/home" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/" element={<Profile />} />
+          
+
           {/* <Route
             path="/test"
             element={
