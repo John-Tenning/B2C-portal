f3c56ca76175d81243a775a94062d7a4f90ecb30
krithik2208 <krithik2208@gmail.com>
2023-02-24 08:48:19 +0530

    Updated Profile

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -2,65 +2,72 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.jpg";
 import { AiFillGithub, AiOutlineMail } from "react-icons/ai";
+import Navbar from "./Navbar";
+import ContactUs from "./ContactUs";
 
 export const Profile = () => {
   return (
     <div className="">
-      <div className="">
-        <nav className="bg-grey flex justify-center items-center">
-          <div className="flex">
-            <div className="h-12 px-2 mt-1">
-              <img src={logo} className="h-full w-auto" />
-            </div>
-            <button className="p-4">Home</button>
-            <button className="p-4">Services</button>
-            <button className="p-4">Explore</button>
-          </div>
-          <div className="flex-1 w-full"></div>
-          <div className="flex">
-            <button className="p-4">Login</button>
-            <button className="p-4">Sign Up</button>
-          </div>
-        </nav>
-      </div>
+      <Navbar />
       <header className="bg-white shadow">
         <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
           <p className="text-xl font-bold text-gray-900">Profile</p>
         </div>
       </header>
       <div className="flex w-screen overflow-hidden h-screen font-lato font-bold">
-        <div className="flex flex-col justify-start items-start w-1/2">
-          <div className={`w-full flex flex-col items-start`}>
-            <p className="font-lato w-full mt-4 p-6">Email Address</p>
+        <div className="flex flex-col justify-center items-center w-[70%] mt-4">
+          <div className="flex-col justify-center items-center">
+            <div className="flex w-full">
+              <div className="">
+                <p className="font-lato">Email address</p>
+              </div>
+              <div className="">
+                <input
+                  type="text"
+                  className="appearance-none outline-none border-b w-full border-b-fog py-4"
+                />
+              </div>
+            </div>
+            <div className="flex w-full">
+              <div className="">
+                <p className="font-lato">Name</p>
+              </div>
+              <div className="">
+                <input
+                  type="text"
+                  className="appearance-none outline-none border-b w-full border-b-fog py-4"
+                />
+              </div>
+            </div>
           </div>
+          <div className="flex-col justify-center items-center">
+            <div className="flex">
+              <div className="">
+                <p className="font-lato">Phone number</p>
+              </div>
+              <div className="">
+                <input
+                  type="text"
+                  className="appearance-none outline-none border-b w-full border-b-fog py-4"
+                />
+              </div>
+            </div>
+            <div className="flex">
+              <div className="">
+                <p className="font-lato">Password</p>
+              </div>
+              <div className="">
+                <input
+                  type="text"
+                  className="appearance-none outline-none border-b w-full border-b-fog py-4"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="w-[30%]"></div>
         </div>
-      </div>
-      <div className="">
-        <footer className=" flex flex-col justify-center items-center font-poppins bg-black p-6">
-          <p className="text-white mb-6">Site developed By</p>
-          <div className="flex flex-col items-center justify-center space-y-2 lg:flex-row lg:space-y-0 lg:space-x-12">
-            <DeveloperItem
-              name="Deepak S G "
-              github_id="deepaksg0210"
-              mail_id="20z213@psgtech.ac.in"
-            />
-            <DeveloperItem
-              name="Krithik R"
-              github_id="krithik2208"
-              mail_id="20z229@psgtech.ac.in"
-            />
-            <DeveloperItem
-              name="R A Sashti Amar"
-              github_id="John-Tenning"
-              mail_id="20z247@psgtech.ac.in"
-            />
-            <DeveloperItem
-              name="Viraj Agarwal"
-              github_id="Viraj-Agarwal"
-              mail_id="20z259@psgtech.ac.in "
-            />
-          </div>
-        </footer>
+        <div className="w-[30%]"></div>
+        <ContactUs />
       </div>
     </div>
   );
