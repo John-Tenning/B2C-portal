9bc3124ff7c6a731d2a6231b2195d37edfdfbef5
John-Tenning <sasti244@gmail.com>
2023-03-31 00:07:14 +0530

    Merge conflicts dynamic

--- frontend/src/components/Profile-corporate.js
+++ frontend/src/components/Profile-corporate.js
@@ -52,11 +52,7 @@ export const Profile = () => {
               <h5 class="mb-1 text-lg font-medium text-gray-900 dark:text-white">
                 Company Name
               </h5>
-<<<<<<< HEAD
-              <span class="text-sm text-gray-500 dark:text-gray-400">
-=======
               <span class="text-l text-gray-500 dark:text-gray-400">
->>>>>>> 5ae8ee84cda22ecdb7ff1c53b25893f72b1df0e8
                 {companyName}
               </span>
             </div>
@@ -66,11 +62,7 @@ export const Profile = () => {
               <h5 class="mb-1 text-lg font-medium text-gray-900 dark:text-white">
                 Email
               </h5>
-<<<<<<< HEAD
               <span class="text-sm text-gray-500 dark:text-gray-400">
-=======
-              <span class="text-l text-gray-500 dark:text-gray-400">
->>>>>>> 5ae8ee84cda22ecdb7ff1c53b25893f72b1df0e8
                 {email}
               </span>
             </div>
@@ -80,11 +72,7 @@ export const Profile = () => {
               <h5 class="mb-1 text-lg font-medium text-gray-900 dark:text-white">
                 Contact
               </h5>
-<<<<<<< HEAD
               <span class="text-sm text-gray-500 dark:text-gray-400">
-=======
-              <span class="text-l text-gray-500 dark:text-gray-400">
->>>>>>> 5ae8ee84cda22ecdb7ff1c53b25893f72b1df0e8
                 {companyContact}
               </span>
             </div>
@@ -96,11 +84,7 @@ export const Profile = () => {
               <h5 class="mb-1 text-lg font-medium text-gray-900 dark:text-white">
                 Company Description
               </h5>
-<<<<<<< HEAD
               <span class="text-sm text-gray-500 dark:text-gray-400">
-=======
-              <span class="text-l text-gray-500 dark:text-gray-400">
->>>>>>> 5ae8ee84cda22ecdb7ff1c53b25893f72b1df0e8
                 {companyDescription}
               </span>
             </div>
@@ -110,36 +94,21 @@ export const Profile = () => {
               <h5 class="mb-1 text-lg font-medium text-gray-900 dark:text-white">
                 Location
               </h5>
-<<<<<<< HEAD
               <span class="text-sm text-gray-500 dark:text-gray-400">
-=======
-              <span class="text-l text-gray-500 dark:text-gray-400">
->>>>>>> 5ae8ee84cda22ecdb7ff1c53b25893f72b1df0e8
                 {companyLocation}
               </span>
             </div>
           </div>
         </div>
-<<<<<<< HEAD
         <div className="flex flex-col items-start w-full lg:w-[45%] ">
           <div class="flex  justify-center  mt-10 h-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-blue-200  w-full">
             <div class="flex flex-col items-center my-8 ">
               <h5 class="mb-10 text-l font-medium text-gray-900 dark:text-white">
-=======
-        <div className="flex flex-col items-start w-[45%] ">
-          <div class="flex  justify-center  mt-10 h-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-blue-200  w-full">
-            <div class="flex flex-col items-center mt-8 ">
-              <h5 class="mb-10 text-lg font-medium text-gray-900 dark:text-white">
->>>>>>> 5ae8ee84cda22ecdb7ff1c53b25893f72b1df0e8
                 Images
               </h5>
               <img
                 class="w-24 h-24 mb-3 rounded-full shadow-lg"
-<<<<<<< HEAD
                 src={images[0]}
-=======
-                src={companyImages}
->>>>>>> 5ae8ee84cda22ecdb7ff1c53b25893f72b1df0e8
                 alt="Dummy image"
               />
             </div>
